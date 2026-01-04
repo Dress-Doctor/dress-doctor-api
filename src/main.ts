eedd0036e04fa6b6c700c9afb317698a9b9719a9
Fedjio Raymond <fedjioraymond@gmail.com>
2026-01-04 07:25:48 +0100

    feat: configured .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const PORT = process.env.PORT ?? 3000;
+  await app.listen(PORT, () => {
+    const logger = new Logger('Bootstrap');
+    logger.log(`Service running on http://localhost:${PORT}`);
+  });
 }
-bootstrap();
+
+bootstrap().catch((err) => console.error(err));
