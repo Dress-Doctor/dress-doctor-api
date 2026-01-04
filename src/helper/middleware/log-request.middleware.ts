0ddbd5cbe19a71374f781b99f440c9c1655f72d8
Fedjio Raymond <fedjioraymond@gmail.com>
2026-01-04 07:34:13 +0100

    feat: add logging middleware and configure CORS and API versioning

---
+++ src/helper/middleware/log-request.middleware.ts
@@ -0,0 +1,11 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request } from 'express';
+
+@Injectable()
+export class LogRequestMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('App');
+  use(req: Request, res: Response, next: NextFunction) {
+    this.logger.log(` ${req.method} ${req.originalUrl}`);
+    next();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Cors
+  app.enableCors({
+    Credential: true,
+    methods: 'GET, HEAD, PUT, POST',
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+  });
+
+  // API Versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
 
   const PORT = process.env.PORT ?? 3000;
   await app.listen(PORT, () => {
